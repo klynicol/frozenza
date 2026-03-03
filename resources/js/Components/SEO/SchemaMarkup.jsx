1ed39fe75c671de989233df6d1be348671753e68
Mark Wickline <mwicklinedev@gmail.com>
2026-03-03 08:29:43 -0600

    fixing product snippets

--- resources/js/Components/SEO/SchemaMarkup.jsx
+++ resources/js/Components/SEO/SchemaMarkup.jsx
@@ -3,22 +3,32 @@ import { Head } from '@inertiajs/react';
 import { getImageUrl } from '@/utils/image';
 
 export default function SchemaMarkup({ type, data }) {
-    const generatePizzaSchema = (pizza) => ({
-        '@context': 'https://schema.org',
-        '@type': 'Product',
-        name: pizza.name,
-        description: pizza.description,
-        image: pizza.image_url,
-        brand: {
-            '@type': 'Brand',
-            name: pizza.brand.name,
-        },
-        aggregateRating: pizza.total_reviews > 0 ? {
-            '@type': 'AggregateRating',
-            ratingValue: pizza.average_rating,
-            reviewCount: pizza.total_reviews,
-        } : undefined,
-        nutrition: pizza?.nutrition_fact ? {
+    const generatePizzaSchema = (pizza) => {
+        const productUrl =
+            typeof window !== 'undefined' && pizza.brand?.slug && pizza.slug
+                ? `${window.location.origin}/pizzas/${pizza.brand.slug}/${pizza.slug}`
+                : undefined;
+        return {
+            '@context': 'https://schema.org',
+            '@type': 'Product',
+            name: pizza.name,
+            description: pizza.description,
+            image: pizza.image_url,
+            brand: {
+                '@type': 'Brand',
+                name: pizza.brand.name,
+            },
+            aggregateRating: pizza.total_reviews > 0 ? {
+                '@type': 'AggregateRating',
+                ratingValue: pizza.average_rating,
+                reviewCount: pizza.total_reviews,
+            } : undefined,
+            offers: {
+                '@type': 'Offer',
+                availability: 'https://schema.org/InStock',
+                ...(productUrl && { url: productUrl }),
+            },
+            nutrition: pizza?.nutrition_fact ? {
             '@type': 'NutritionInformation',
             calories: pizza.nutrition_fact.calories != null ? `${pizza.nutrition_fact.calories} calories` : undefined,
             proteinContent: pizza.nutrition_fact.protein ?? undefined,
@@ -40,7 +50,8 @@ export default function SchemaMarkup({ type, data }) {
             vitaminAContent: pizza.nutrition_fact.vitamin_a ?? undefined,
             vitaminCContent: pizza.nutrition_fact.vitamin_c ?? undefined,
         } : undefined,
-    });
+        };
+    };
 
     const generateBrandSchema = (brand) => ({
         '@context': 'https://schema.org',
